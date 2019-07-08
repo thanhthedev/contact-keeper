b5c64669d391936d94aa43af16113e5f0d5cc373
thanhthedev <huythanhdao@gmail.com>
2019-07-08 16:30:04 +1000

    added register and login page + alerts

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,75 @@
+import React, { useReducer } from "react";
+import AuthContext from "./authContext";
+import AuthReducer from "./authReducer";
+import axios from "axios";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  lOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from "../types";
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+    user: null
+  };
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  // Load User
+  const loadUser = () => console.log("load user");
+  // Register User
+  const register = async formData => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.post("/api/users", formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
+  // Login User
+  const login = () => console.log("login user");
+  // Logout User
+  const logout = () => console.log("logout user");
+  // CLear Errors
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+        register,
+        login,
+        logout,
+        clearErrors,
+        loadUser
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
