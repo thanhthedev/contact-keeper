d867c0c7816daa5d8e3c5f766e916d8e738a26d1
thanhthedev <huythanhdao@gmail.com>
2019-07-10 16:55:53 +1000

    1st deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -18,5 +19,14 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in production
+if (proccess.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
