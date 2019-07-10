cf0a6247494eaf9522e7537403afac8623cfcf8b
thanhthedev <huythanhdao@gmail.com>
2019-07-10 17:02:33 +1000

    fixed problem

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5002;
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the contact keeper api" })
-);
-
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
